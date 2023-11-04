826d286b7bd32d697211ade46aecb2036cb5cfbd
LorenzoVarese <lorenzo.varese@usi.ch>
2023-11-04 22:30:40 +0100

    Add real example of update

--- data_manipulation/update_data.py
+++ data_manipulation/update_data.py
@@ -4,20 +4,29 @@ def update_data(query, new_values):
     client = create_connection()
     db = client['TripAdvisor']
     collection = db['EuropeanRestaurants']
-    
+
     update_result = collection.update_many(query, {'$set': new_values})
     print(f'Updated {update_result.modified_count} documents.')
 
+def update_open_hours_multiple_days(restaurant_link, days, time_ranges):
+    """
+    Update the opening hours for specific days for a restaurant.
+
+    :param restaurant_link: The unique identifier for the restaurant.
+    :param days: A list of days to update the hours for.
+    :param time_ranges: A list of strings containing the new time ranges for the days.
+    """
+    query = {'restaurant_link': restaurant_link}
+    # The time_ranges list is assumed to be the same for all days to be updated.
+    # Convert the list of time ranges into a format that is a list of strings, which each day will use.
+    time_ranges_str = time_ranges
+    
+    new_values = {f'availability.original_open_hours.{day}': time_ranges_str for day in days}
+    
+    # Execute the update operation.
+    update_data(query, new_values)
+
 # Example usage
 if __name__ == "__main__":
-    query = {'restaurant_link': 'g187079-d1234567'} # 'La Bonne Fourchette'
-    new_values = {
-        'availability.original_open_hours.Mon': ['08:00-22:30'],
-        'availability.original_open_hours.Tue': ['08:00-22:30'],
-        'availability.original_open_hours.Wed': ['08:00-22:30'],
-        'availability.original_open_hours.Thu': ['08:00-22:30'],
-        'availability.original_open_hours.Fri': ['08:00-22:30'],
-        'availability.original_open_hours.Sat': ['08:00-22:30'],
-    }
-    
-    update_data(query, new_values)
+    # Update the hours for Monday, Tuesday, and Friday to '08:00-14:00' and '17:00-22:30'.
+    update_open_hours_multiple_days('g187079-d1234567', ['Mon', 'Tue', 'Fri'], ['08:00-14:00', '17:00-22:30'])
