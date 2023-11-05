f617b4a5e2c29c100070ef390ee937be2d3ee8b8
LorenzoVarese <lorenzo.varese@usi.ch>
2023-11-05 09:30:42 +0100

    Add data maniupulation query

--- data_manipulation/read_data.py
+++ data_manipulation/read_data.py
@@ -1,6 +1,6 @@
 from db_connection.mongo_connect import create_connection
 
-def find_data(query):
+def find_and_print_data(query):
     client = create_connection()
     db = client['TripAdvisor']
     collection = db['EuropeanRestaurants']
@@ -10,7 +10,15 @@ def find_data(query):
     for document in documents:
         print(document)
 
+def find_restaurant_by_link(restaurant_link):
+    client = create_connection()
+    db = client['TripAdvisor']
+    collection = db['EuropeanRestaurants']
+
+    restaurant = collection.find_one({'restaurant_link': restaurant_link}, {'rating': 1})
+    return restaurant
+
 # Example usage
 if __name__ == "__main__":
     query = {'restaurant_link': 'g187079-d1234567'} # 'La Bonne Fourchette'
-    find_data(query)
+    find_and_print_data(query)
--- data_manipulation/update_data.py
+++ data_manipulation/update_data.py
@@ -1,6 +1,16 @@
+from data_manipulation.read_data import find_restaurant_by_link
 from db_connection.mongo_connect import create_connection
+from bson.decimal128 import Decimal128
 
-def update_data(query, new_values):
+def db_update_one(query, new_values):
+    client = create_connection()
+    db = client['TripAdvisor']
+    collection = db['EuropeanRestaurants']
+
+    update_result = collection.update_one(query, {'$set': new_values})
+    print(f'Updated {update_result.modified_count} documents.')
+
+def db_update_many(query, new_values):
     client = create_connection()
     db = client['TripAdvisor']
     collection = db['EuropeanRestaurants']
@@ -24,9 +34,46 @@ def update_open_hours_multiple_days(restaurant_link, days, time_ranges):
     new_values = {f'availability.original_open_hours.{day}': time_ranges_str for day in days}
     
     # Execute the update operation.
-    update_data(query, new_values)
+    db_update_one(query, new_values)
 
-# Example usage
+
+def update_ratings(restaurant_link, rating_update):
+    # Retrieve the current rating details
+    restaurant = find_restaurant_by_link(restaurant_link)
+
+    if restaurant and 'rating' in restaurant:
+        current_ratings = restaurant['rating']
+
+        # Calculate the new total reviews count and update the specific rating.
+        total_reviews_count = current_ratings.get('total_reviews_count', Decimal128('0')).to_decimal() + 1
+        current_ratings[rating_update] = current_ratings.get(rating_update, Decimal128('0')).to_decimal() + 1
+
+        # Recalculate the average rating.
+        ratings_weight = {'excellent': 5, 'very_good': 4, 'average': 3, 'poor': 2, 'terrible': 1}
+        total_rating_score = sum(ratings_weight[key] * current_ratings.get(key, Decimal128('0')).to_decimal() for key in ratings_weight)
+        avg_rating = total_rating_score / total_reviews_count
+
+        # Prepare the new values for updating the document.
+        new_values = {
+            'rating.total_reviews_count': Decimal128(str(total_reviews_count)),
+            'rating.avg_rating': Decimal128(str(avg_rating)),
+            f'rating.{rating_update}': Decimal128(str(current_ratings[rating_update]))
+        }
+
+        db_update_one({'restaurant_link': restaurant_link}, new_values)
+
+    else:
+        # Handle the case where the restaurant is not found or has no ratings.
+        return 'Restaurant not found or has no ratings yet.'
+
+
+# Example usage: update_ratings
+# if __name__ == "__main__":
+#     # Example to increment the 'excellent' rating
+#     update_ratings('g1005749-d14139205', 'excellent')
+
+
+# Example usage: update_open_hours_multiple_days
 if __name__ == "__main__":
     # Update the hours for Monday, Tuesday, and Friday to '08:00-14:00' and '17:00-22:30'.
     update_open_hours_multiple_days('g187079-d1234567', ['Mon', 'Tue', 'Fri'], ['08:00-14:00', '17:00-22:30'])
